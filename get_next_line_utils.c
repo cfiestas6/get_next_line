74496143787cd009892fdbde9e77c5140ab9ffa7
carlos <cfiestas6@gmail.com>
2023-02-22 19:33:39 +0100

    updates

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,8 +5,7 @@ size_t	ft_strlen(const char *str)
 	size_t	n;
 
 	n = 0;
-	while (str[n])
-		n++;
+	while (str[n++]);
 	return (n);
 }
 
@@ -14,12 +13,9 @@ static size_t	ft_getindex(size_t s1_len, char *r, char const *s1)
 {
 	size_t	i;
 
-	i = 0;
-	while (i < s1_len)
-	{
+	i = -1;
+	while (++i < s1_len)
 		r[i] = s1[i];
-		i++;
-	}
 	return (i);
 }
 
@@ -36,14 +32,16 @@ char	*ft_joinfree(char *s1, char *s2)
 	int j;
 	char *result;
 
-	i = 0;
 	if (!s1 || !s2)
 		return (ft_freestr(s1, s2));
 	result = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!result)
 		return (ft_freestr(s1, s2));
 	j = 0;
-	i = ft_getindex(ft_strlen(s1), result, s1);
+	// i = ft_getindex(ft_strlen(s1), result, s1);
+	i = -1;
+	while (++i < ft_strlen(s1))
+		r[i] = s1[i];
 	while (j < ft_strlen(s2))
 		result[i++] = s2[j++];
 	result[i] = 0;
